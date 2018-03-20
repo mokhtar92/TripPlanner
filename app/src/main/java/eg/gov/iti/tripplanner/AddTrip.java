f5d0500338f3f8f01235b9682ea015b424043e8b
IbrahimDesouky72 <Ibrahim.desouky44@gmail.com>
2018-03-20 21:21:15 +0200

    On Item Click action Method

---
+++ app/src/main/java/eg/gov/iti/tripplanner/AddTrip.java
@@ -0,0 +1,13 @@
+package eg.gov.iti.tripplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+}
