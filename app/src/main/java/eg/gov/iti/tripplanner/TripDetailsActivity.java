b35c8aa194db83d705a54f91574136c75db32b95
ahmed-mokhtar <31440751+ahmed-mokhtar@users.noreply.github.com>
2018-03-30 20:27:11 +0200

    complete Details Activity

--- app/src/main/java/eg/gov/iti/tripplanner/TripDetailsActivity.java
+++ app/src/main/java/eg/gov/iti/tripplanner/TripDetailsActivity.java
@@ -5,23 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
+
 import eg.gov.iti.tripplanner.model.Trip;
 
 public class TripDetailsActivity extends AppCompatActivity {
-    Intent intent;
-    TextView tripName;
-    TextView tripDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_details);
 
-        intent = getIntent();
-        if (intent != null) {
-            Trip trip = intent.getParcelableExtra("trip");
-            if (trip != null) {
+        TextView tripDate, tripTime, tripName, tripFrom, tripTo, tripNotes;
 
+        tripDate = findViewById(R.id.details_trip_date);
+        tripTime = findViewById(R.id.details_trip_time);
+        tripName = findViewById(R.id.details_trip_name);
+        tripFrom = findViewById(R.id.details_trip_from);
+        tripTo = findViewById(R.id.details_trip_to);
+        tripNotes = findViewById(R.id.details_trip_notes);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            Trip trip = intent.getParcelableExtra("tripDetails");
+            if (trip != null) {
+                tripDate.setText(trip.getTripDate());
+                tripTime.setText(trip.getTripTime());
+                tripName.setText(trip.getTripName());
+                tripFrom.setText(trip.getStartName());
+                tripTo.setText(trip.getEndName());
+
+                List<String> notes = trip.getNotes();
+                for (String note : notes) {
+                    tripNotes.append("-" + note.concat("\n"));
+                }
             }
         }
 
