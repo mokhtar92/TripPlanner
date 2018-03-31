820f6bddbb5fe90a274b4e5520474a693109001e
ahmed-mokhtar <31440751+ahmed-mokhtar@users.noreply.github.com>
2018-03-31 19:07:49 +0200

    Add time & date to details activity

--- app/src/main/java/eg/gov/iti/tripplanner/SplashActivity.java
+++ app/src/main/java/eg/gov/iti/tripplanner/SplashActivity.java
@@ -9,10 +9,15 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static boolean isFirstTime = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        if (isFirstTime){
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+            isFirstTime = false;
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
